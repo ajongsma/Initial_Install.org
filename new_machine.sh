66104d6b4cef33647a4513e9f9b742bfa49449e4
Andries Jongsma <a.jongsma@gmail.com>
2013-01-13 22:26:32 +0100

    Update
    Signed-off-by: Andries Jongsma <a.jongsma@gmail.com>

---
+++ new_machine.sh
@@ -0,0 +1,39 @@
+# install xcode via App Store first! It takes awhile
+# install the command-line tools as well
+
+#open http://itunes.apple.com/us/app/xcode/id497799835?mt=12
+if [ ! -e /Applications/Xcode.app ] ; then
+    open http://itunes.apple.com/us/app/xcode/id497799835?mt=12
+  
+    echo Xcode not installed, please install...
+    sleep 15
+    quit
+else
+    echo Xcode found
+fi
+
+gcc_versionlong=`gcc --version`
+gcc_version=${gcc_versionlong:29:1}
+if [ $gcc_version -eq 4 ] ; then
+  echo gcc $gcc_version found
+else
+  echo gcc not found, please install the Xcode command line tools...
+fi
+
+# https://github.com/roderik/dotfiles
+# https://github.com/mathiasbynens/dotfiles
+git clone https://roderik@github.com/roderik/dotfiles.git
+cd dotfiles
+
+if [ ! -e .extra ] ; then
+    cp .extra.dist .extra
+    nano .extra
+else
+    echo .xtra file found
+fi
+nano .osx
+./.osx
+./.brew
+
+./bootstrap.sh
+

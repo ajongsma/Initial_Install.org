a5a51d5f498d95337045b7519fb4e04533d862c1
ajongsma <a.jongsma@gmail.com>
2013-01-15 11:26:39 +0100

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+#------------------------------------------------------------------------------
+# References:
+# https://gist.github.com/4135542
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# SETTINGS
+#------------------------------------------------------------------------------
+ 
+MYSQL_ROOT_PASSWORD=password
+MYSQL_GITORIOUS_PASSWORD=password
+GITORIOUS_HOST=gitorious
+SYSADMIN=sysadmin
+SYSADMIN_EMAIL=git@$GITORIOUS_HOST
+SYSADMIN_PASSWORD=password
+
+#------------------------------------------------------------------------------
+# CUSTOM FUNCTION
+#------------------------------------------------------------------------------
+
 successfully() {
   $* || (echo "failed" 1>&2 && exit 1)
 }
+
+#------------------------------------------------------------------------------
+# INSTALLATION STARTS HERE
+#------------------------------------------------------------------------------
+
+mkdir ~/src
+cd ~/src
 
 ###### To test #######
 ## Permissions
@@ -39,7 +67,7 @@ while ( [ ! -e /Applications/Xcode.app ] )
 end
 
 
-# TESTING - require instead of specific chars
+# TESTING - require instead of specific chars - START
 require 'gcc'
 #gcc_versionlong=`gcc --version`
 #gcc_version=${gcc_versionlong:29:1}
@@ -48,6 +76,7 @@ require 'gcc'
 #else
 #  echo gcc not found, please install the Xcode command line tools...
 #fi
+# TESTING - require instead of specific chars - END
 
 # https://github.com/roderik/dotfiles
 # https://github.com/mathiasbynens/dotfiles
@@ -78,4 +107,40 @@ fi
 #./.iTerm2
 
 #./bootstrap.sh
+
+###### TESTING - ADDENDUM CODE 1 ###### START
+## MySQL
+#echo "mysql-server-5.5 mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+#echo "mysql-server-5.5 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+#echo "mysql-server-5.5 mysql-server/root_password seen true" | sudo debconf-set-selections
+#echo "mysql-server-5.5 mysql-server/root_password_again seen true" | sudo debconf-set-selections
+# 
+#sudo apt-get install -y mysql-client-5.5 mysql-server-5.5 libmysqlclient-dev
+#mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE gitorious"
+#mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER 'gitorious'@'localhost' IDENTIFIED BY '$MYSQL_GITORIOUS_PASSWORD'"
+#mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON gitorious.* TO 'gitorious'@'localhost'"
+ 
+## Sphinx
+#cd ~/src
+#wget -c http://sphinxsearch.com/files/sphinx-0.9.9.tar.gz
+#tar zxf sphinx-0.9.9.tar.gz
+#cd sphinx-0.9.9
+#./configure
+#make 
+#sudo make install
+# 
+## Memcached 
+#sudo update-rc.d memcached defaults
+# 
+## Adding gitorious group
+#sudo groupadd gitorious
+#sudo usermod -a -G gitorious $SYSADMIN
+# 
+## Cloning gitorious project
+# 
+#sudo git clone https://git.gitorious.org/gitorious/mainline.git /var/www/gitorious
+#sudo chown -R $SYSADMIN:gitorious /var/www/gitorious
+#sudo chmod -R g+sw /var/www/gitorious
+
+###### TESTING - ADDENDUM CODE 1 ###### END
 
